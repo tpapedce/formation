d019f3e16c2ed52cd62e6a8e6df5d20a36d9ab36
tpapedce <t.pape@dreamcentury.com>
2017-02-28 10:55:06 +0100

    avant d'attaquer les commentaires

---
+++ App/Frontend/Modules/News/NewsController.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\Frontend\Modules\News;
+
+use \OCFram\BackController;
+use \OCFram\HTTPRequest;
+
+class NewsController extends BackController
+{
+	public function executeIndex(HTTPRequest $request)
+	{
+		$nombreNews = $this->app->config()->get('nombre_news');
+		$nombreCaracteres = $this->app->config()->get('nombre_caracteres');
+		
+		// On ajoute une définition pour le titre.
+		$this->page->addVar('title', 'Liste des '.$nombreNews.' dernières news');
+		
+		// On récupère le manager des news.
+		$manager = $this->managers->getManagerOf('News');
+		
+		$listeNews = $manager->getList(0, $nombreNews);
+		
+		foreach ($listeNews as $news)
+		{
+			if (strlen($news->contenu()) > $nombreCaracteres)
+			{
+				$debut = substr($news->contenu(), 0, $nombreCaracteres);
+				$debut = substr($debut, 0, strrpos($debut, ' ')) . '...';
+				
+				$news->setContenu($debut);
+			}
+		}
+		
+		// On ajoute la variable $listeNews à la vue.
+		$this->page->addVar('listeNews', $listeNews);
+	}
+	
+	public function executeShow(HTTPRequest $request)
+	{
+		$news = $this->managers->getManagerOf('News')->getUnique($request->getData('id'));
+		
+		if (empty($news))
+		{
+			$this->app->httpResponse()->redirect404();
+		}
+		
+		$this->page->addVar('title', $news->titre());
+		$this->page->addVar('news', $news);
+	}
+}
