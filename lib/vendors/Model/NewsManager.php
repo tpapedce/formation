223ef8a1a29ffe989b99818f9b8c148befa882d9
tpapedce <t.pape@dreamcentury.com>
2017-02-28 12:52:10 +0100

    avant les commentaires backend

--- lib/vendors/Model/NewsManager.php
+++ lib/vendors/Model/NewsManager.php
@@ -6,6 +6,38 @@ use \OCFram\Manager;
 abstract class NewsManager extends Manager
 {
 	/**
+	 * Méthode permettant d'ajouter une news.
+	 * @param $news News La news à ajouter
+	 * @return void
+	 */
+	abstract protected function add(News $news);
+	
+	/**
+	 * Méthode permettant d'enregistrer une news.
+	 * @param $news News la news à enregistrer
+	 * @see self::add()
+	 * @see self::modify()
+	 * @return void
+	 */
+	public function save(News $news)
+	{
+		if ($news->isValid())
+		{
+			$news->isNew() ? $this->add($news) : $this->modify($news);
+		}
+		else
+		{
+			throw new \RuntimeException('La news doit être validée pour être enregistrée');
+		}
+	}
+	
+	/**
+	 * Méthode renvoyant le nombre de news total.
+	 * @return int
+	 */
+	abstract public function count();
+	
+	/**
 	 * Méthode retournant une liste de news demandée.
 	 * @param $debut int La première news à sélectionner
 	 * @param $limite int Le nombre de news à sélectionner
@@ -19,4 +51,13 @@ abstract class NewsManager extends Manager
 	 * @return News La news demandée
 	 */
 	abstract public function getUnique($id);
+	
+	/**
+	 * Méthode permettant de modifier une news.
+	 * @param $news news la news à modifier
+	 * @return void
+	 */
+	abstract protected function modify(News $news);
+	
+	abstract public function delete($id);
 }
